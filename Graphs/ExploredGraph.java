66b949ea269d09602d48b917393b2309a4cdd76a
Andrew Bayles <andrewbayles1@gmail.com>
2016-03-02 20:36:14 -0800

    Finished just need to comment

--- Graphs/ExploredGraph.java
+++ Graphs/ExploredGraph.java
@@ -3,6 +3,7 @@ import java.util.Objects;
 import java.util.Queue;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
@@ -36,22 +37,22 @@ import java.util.function.Function;
 public class ExploredGraph {
 	Set<Vertex> Ve; // collection of explored vertices
 	Set<Edge> Ee;   // collection of explored edges
-	Operator o01; //
-	Operator o02;
-	Operator o10;
-	Operator o12;
-	Operator o20;
-	Operator o21;
+	Vertex startV;
+	ArrayList<Operator> ops;
 	
 	public ExploredGraph() {
 		Ve = new LinkedHashSet<Vertex>();
 		Ee = new LinkedHashSet<Edge>();
-		o01 = new Operator(0,1);
-		o02 = new Operator(0,2);
-		o10 = new Operator(1,0);
-		o12 = new Operator(1,2);
-		o21 = new Operator(2,0);
-		o20 = new Operator(2,1);
+		ops = new ArrayList<Operator>();
+		for(int i = 0; i < 3;i++){
+			for(int j = 0; j< 3; j++){
+				if(i != j){
+					System.out.println(i + j);
+					Operator op = new Operator(i,j);
+					ops.add(op); 
+				}
+			}
+		}
 	}
 
 	public void initialize() {
@@ -68,52 +69,106 @@ public class ExploredGraph {
 	}    
 	
 	public void dfs(Vertex vi, Vertex vj) {
-		
-	} // Implement this.
-	
-	public void bfs(Vertex vi, Vertex vj) {
-			
 		initialize();
-		Vertex start = vi;
-		Vertex end = vj; 
+		startV = vi;
 		Ve.add(vi);
-		Queue<Vertex> aux = new LinkedList<Vertex>();
+		Stack<Vertex> aux = new Stack<Vertex>();
 		aux.add(vi);
+		System.out.println("Operators:" + ops.toString());
 		while(!aux.isEmpty()){
-			Vertex current = aux.remove();
-			for(int i = 0; i < 2;i++){
-				for(int j = 0; j< 2; j++){
-					if(i != j){
-						Operator op = new Operator(i,j);
+			Vertex current = aux.pop();
+			for(int i = 0; i < ops.size();i++){
+				Operator op = ops.get(i);				
+				if(op.getPrecondition(current)){
+					Vertex temp = op.getTransition(current);
+					if(!Ve.contains(temp)){
+						System.out.println("inital state: " + current.toString());
+						System.out.println("After transition state: " + temp.toString());
+						System.out.println("NVerticies: " + nvertices());
+						System.out.println("NEdges: " + nedges());
 						
-						if(op.getPrecondition(current)){
-							Ve.add(current);
-							Vertex temp = op.getTransition(current);
-							if(!Ve.contains(temp)){
-							
-							System.out.println("inital state: " + current.toString());
-							System.out.println("After transition state: " + temp.toString());
-							Ee.add(new Edge(current, temp)); 
-							Ve.add(temp);
-							aux.add(temp);
-							if(temp.equals(vj)){aux.clear();}
-							}
-						}
+						Ee.add(new Edge(current, temp)); 
+						Ve.add(temp);
+						aux.push(temp);
+						System.out.println("Aux is:" + aux.toString());
+						if(temp.equals(vj)){aux.clear(); break;}
 					}
 				}
 			}
 		}
-			
-		
 	} // Implement this.
 	
+	//Performs a Breadth First Search from the two parameters passed. The start and end
+	// point for the search.
+	public void bfs(Vertex vi, Vertex vj) {
+		initialize();
+		startV = vi;
+		Ve.add(vi);
+		Queue<Vertex> aux = new LinkedList<Vertex>();//set up storage
+		aux.add(vi);
+		System.out.println("Operators:" + ops.toString());
+		while(!aux.isEmpty()){
+			Vertex current = aux.remove();//get new node to check possible states
+			for(int i = 0; i < ops.size();i++){
+				Operator op = ops.get(i);				
+				if(op.getPrecondition(current)){//checks if legal move
+					Vertex temp = op.getTransition(current);
+					if(!Ve.contains(temp)){//stops backtracking
+						System.out.println("inital state: " + current.toString());
+						System.out.println("After transition state: " + temp.toString());
+						System.out.println("NVerticies: " + nvertices());
+						System.out.println("NEdges: " + nedges());
+						Ee.add(new Edge(current, temp)); 
+						Ve.add(temp);
+						aux.add(temp);
+						System.out.println("Aux is:" + aux.toString());
+						if(temp.equals(vj)){aux.clear(); break;}
+					}
+				}
+			}
+		}
+		
+	} 
+	
 	public ArrayList<Vertex> retrievePath(Vertex vi) {
-		return null;
-	} // Implement this.
+		if (!Ve.contains(vi)) {
+			return null;
+		}
+		ArrayList<Vertex> path = new ArrayList<Vertex>();
+		Edge viEdge = null;
+		ArrayList<Edge> rEdge = new ArrayList<Edge>();
+		for (Edge edge : Ee) {
+			rEdge.add(edge);
+			if (edge.getEndpoint2().equals(vi)) {
+				viEdge = edge;
+			}
+		}
+		Collections.reverse(rEdge); // switches the order so the last edge is first
+		Vertex leftVertex = viEdge.getEndpoint1();
+		Vertex rightVertex = viEdge.getEndpoint2();
+		Vertex lastLeftVertex = leftVertex;
+
+		path.add(rightVertex);
+		boolean atSolutionEdge = true;
+		for (Edge edge: rEdge) {
+			if (atSolutionEdge) {
+				atSolutionEdge = false;
+				continue;
+			}
+			if (edge.getEndpoint2().equals(lastLeftVertex)) {
+				path.add(edge.getEndpoint2());
+				lastLeftVertex = edge.getEndpoint1();
+			}
+		}
+		path.add(startV);
+		Collections.reverse(path);
+		return path;
+	}
 	
 	
 	public ArrayList<Vertex> shortestPath(Vertex vi, Vertex vj) {
-		return null;
+		this.bfs(vi, vj);
+		return this.retrievePath(vj);
 	} // Implement this.
 	
 	public Set<Vertex> getVertices() {return Ve;} 
@@ -126,21 +181,23 @@ public class ExploredGraph {
 		// Test the vertex constructor: 
 		Vertex v0 = eg.new Vertex("[[4,3,2,1],[],[]]");
 		Vertex v1 = eg.new Vertex("[[],[],[4,3,2,1]]");
-		System.out.println(v0);
-		System.out.println(v1);
+//		System.out.println(v0);
+//		System.out.println(v1);
 		Edge e1 = eg.new Edge(v0, v1);
-		System.out.println(e1.toString());
-		System.out.println(e1.getEndPoint1());
-		System.out.println(e1.getEndPoint2());
-		Operator op1 = eg.new Operator(2,0);
-		boolean x = op1.getPrecondition(v1);
-		if(x){
-			op1.getTransition(v1);
-		}else{
-			System.out.println("Wrong Precondition");
-		}
+//		System.out.println(e1.toString());
+//		System.out.println(e1.getEndPoint1());
+//		System.out.println(e1.getEndPoint2());
+//		Operator op1 = eg.new Operator(2,0);
+//		boolean x = op1.getPrecondition(v1);
+//		if(x){
+//			op1.getTransition(v1);
+//		}else{
+//			System.out.println("Wrong Precondition");
+//		}
 		
-		eg.bfs(v0,v1);
+		//eg.bfs(v0,v1);
+		//eg.dfs(v0, v1);
+		System.out.println(eg.shortestPath(v0,v1));
 		System.out.println("Tests Finished");
 		// Add your own tests here.
 		// The autograder code will be used to test your basic functionality later.
@@ -161,12 +218,12 @@ public class ExploredGraph {
 					parts[i]=parts[i].replaceAll("\\[","");
 					parts[i]=parts[i].replaceAll("\\]","");
 					List<String> al = new ArrayList<String>(Arrays.asList(parts[i].split(",")));
-					System.out.println("ArrayList al is: "+al);
+					//System.out.println("ArrayList al is: "+al);
 					Iterator<String> it = al.iterator();
 					while (it.hasNext()) {
 						String item = it.next();
                                                 if (!item.equals("")) {
-                                                        System.out.println("item is: "+item);
+                                                     //   System.out.println("item is: "+item);
                                                         pegs.get(i).push(Integer.parseInt(item));
                                                 }
 					}
@@ -190,7 +247,7 @@ public class ExploredGraph {
 					if (this == o) return true;
 					if (o == null || getClass() != o.getClass()) return false;
 					Vertex vertex = (Vertex) o;
-					return o.toString().equals(this.toString());
+					return vertex.toString().equals(this.toString());
 				}
 				
 				// hashes values for the LinkedHashSet
@@ -211,11 +268,11 @@ public class ExploredGraph {
 			v2 = vj;
 		}
 		
-		public Vertex getEndPoint1(){
+		public Vertex getEndpoint1(){
 			return v1;
 		}
 		
-		public Vertex getEndPoint2(){
+		public Vertex getEndpoint2(){
 			return v2;
 		}
 		
